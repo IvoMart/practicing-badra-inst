949d232f2f0f7233869d76bdba7fb39aed8dcb5a
IvoMart <ivo_ivancho@hotmail.com>
2024-07-24 21:33:29 -0300

    Ejercicio 1 - JS

---
+++ Js/Ejercicios.js
@@ -0,0 +1,32 @@
+ /// Ejercicio 1: Hola Mundo 
+ // Escribe un programa que imprima "Hola, Mundo!" en la consola.
+const ecribeConsola = ()=> console.log('Hola Mundo')
+
+ /// Ejercicio 2: Suma de dos números 
+ // Escribe un programa que solicite al usuario dos números y los sume.
+
+
+ /// Ejercicio 3: Par o lmpar 
+ // Escribe un programa que determine si un número ingresado por el usuario es par o impar.
+
+ /// Ejercicio 4: Máximo de tres números 
+ // Escribe un programa que solicite al usuario tres números y determine cuál es el mayor.
+
+ /// Ejercicio 5: Factorial de un número 
+ // Escribe un programa que calcule el factorial de un número ingresado por el usuario.
+
+ 
+ /// Ejercicio 6: Contar vocales en una cadena 
+ // Escribe un programa que cuente el número de vocales en una cadena ingresada por el usuario.
+ 
+ /// Ejercicio 7: Numeros primos 
+ // Escribe un programa que determine si un número ingresado por el usuario es primo.
+ 
+ /// Ejercicio 8: Serie de Fibonacci 
+ // Escribe un programa que genere la serie de Fibonacci hasta un número n ingresado por el usuario.
+ 
+ /// Ejercicio 9: Invertir una cadena 
+ // Escribe un programa que invierta una cadena de texto ingresada por el usuario.
+ 
+ /// Ejercicio 10: Suma de dígitos 
+ // Escribe un programa que sume los dígitos de un número entero ingresado por el usuario.
